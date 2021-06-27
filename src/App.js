8e84661e34e38c1050eeca6567192b4beeea9063
Rubén Ortiz <runerd.menethil@gmail.com>
2021-06-27 06:58:53 -0400

    Add CategoriesSelector component to the app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./styles/App.css";
 
 import Navbar from "./components/Navbar";
+import CategoriesSelector from "./components/CategoriesSelector";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <CategoriesSelector />
     </div>
   );
 }
