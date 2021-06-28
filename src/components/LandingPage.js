5a2473a84204abef895d7d03c6b65245961bfba1
Rubén Ortiz <runerd.menethil@gmail.com>
2021-06-28 03:10:10 -0400

    Create LandingPage component to wrap all the content

---
+++ src/components/LandingPage.js
@@ -0,0 +1,12 @@
+import PromoHeader from "./PromoHeader";
+import "../styles/LandingPage.css";
+
+const LandingPage = () => {
+  return (
+    <div className="landing-page">
+      <PromoHeader />
+    </div>
+  );
+};
+
+export default LandingPage;
