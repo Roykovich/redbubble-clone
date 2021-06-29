983fa5740007329447552e382dc959bb7fad45dc
Rubén Ortiz <runerd.menethil@gmail.com>
2021-06-29 04:51:22 -0400

    Add ProductRange to the LandingPage component

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,10 +1,12 @@
 import PromoHeader from "./PromoHeader";
+import ProductRange from "./ProductRange";
 import "../styles/LandingPage.css";
 
 const LandingPage = () => {
   return (
     <div className="landing-page">
       <PromoHeader />
+      <ProductRange />
     </div>
   );
 };
