3c40e7fc83a1d8af20440bdfc7cb014a454ccd13
Rubén Ortiz <runerd.menethil@gmail.com>
2021-07-01 02:38:31 -0400

    Create Featured product card structure

--- src/components/FeaturedCarousel.js
+++ src/components/FeaturedCarousel.js
@@ -1,11 +1,24 @@
 import "../styles/FeaturedCarousel.css";
 
+import product from "../images/products/black_pillow.jpg";
+
 const FeaturedCarousel = () => {
   return (
     <section>
       <div className="featured-products-container">
         <h1>Featured products</h1>
-        <div className="featured-products-carousel"></div>
+        <div className="featured-products-carousel">
+          <div className="featured-product-card">
+            <div>
+              <img src={product} alt="Black Pillow" />
+            </div>
+            <div>
+              <h5>Product Name</h5>
+              <p>by John Doe</p>
+              <h4>From $4.20</h4>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
