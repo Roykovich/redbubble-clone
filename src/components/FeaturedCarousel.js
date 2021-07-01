4c814ee9cd5c6c60b21757cca898178f39cbb32e
Rubén Ortiz <runerd.menethil@gmail.com>
2021-06-30 23:42:20 -0400

    Create FeaturedCarousel component

---
+++ src/components/FeaturedCarousel.js
@@ -0,0 +1,14 @@
+import "../styles/FeaturedCarousel.css";
+
+const FeaturedCarousel = () => {
+  return (
+    <section>
+      <div className="featured-products-container">
+        <h1>Featured products</h1>
+        <div className="featured-products-carousel"></div>
+      </div>
+    </section>
+  );
+};
+
+export default FeaturedCarousel;
