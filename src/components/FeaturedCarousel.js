95f413c7d5e2ddd660d3a2e003567ee7d14202d8
Rubén Ortiz <runerd.menethil@gmail.com>
2021-07-03 22:34:39 -0400

    Add columns that has 2 rows of featured cards

--- src/components/FeaturedCarousel.js
+++ src/components/FeaturedCarousel.js
@@ -1,4 +1,5 @@
 import "../styles/FeaturedCarousel.css";
+import FeaturedCard from "./FeaturedCard";
 
 import product from "../images/products/black_pillow.jpg";
 
@@ -8,15 +9,149 @@ const FeaturedCarousel = () => {
       <div className="featured-products-container">
         <h1>Featured products</h1>
         <div className="featured-products-carousel">
-          <div className="featured-product-card">
-            <div>
-              <img src={product} alt="Black Pillow" />
-            </div>
-            <div>
-              <h5>Product Name</h5>
-              <p>by John Doe</p>
-              <h4>From $4.20</h4>
-            </div>
+          <div className="featured-column">
+            <FeaturedCard
+              alt={"Black Pillow"}
+              artist={"John Dow"}
+              cost={6.66}
+              image={product}
+              productName={"Incrediblo Black Pillow"}
+            />
+            <FeaturedCard
+              alt={"Black Pillow"}
+              artist={"John Dow"}
+              cost={6.66}
+              image={product}
+              productName={"Incrediblo Black Pillow"}
+            />
+          </div>
+          <div className="featured-column">
+            <FeaturedCard
+              alt={"Black Pillow"}
+              artist={"John Dow"}
+              cost={6.66}
+              image={product}
+              productName={"Incrediblo Black Pillow"}
+            />
+            <FeaturedCard
+              alt={"Black Pillow"}
+              artist={"John Dow"}
+              cost={6.66}
+              image={product}
+              productName={"Incrediblo Black Pillow"}
+            />
+          </div>
+          <div className="featured-column">
+            <FeaturedCard
+              alt={"Black Pillow"}
+              artist={"John Dow"}
+              cost={6.66}
+              image={product}
+              productName={"Incrediblo Black Pillow"}
+            />
+            <FeaturedCard
+              alt={"Black Pillow"}
+              artist={"John Dow"}
+              cost={6.66}
+              image={product}
+              productName={"Incrediblo Black Pillow"}
+            />
+          </div>
+          <div className="featured-column">
+            <FeaturedCard
+              alt={"Black Pillow"}
+              artist={"John Dow"}
+              cost={6.66}
+              image={product}
+              productName={"Incrediblo Black Pillow"}
+            />
+            <FeaturedCard
+              alt={"Black Pillow"}
+              artist={"John Dow"}
+              cost={6.66}
+              image={product}
+              productName={"Incrediblo Black Pillow"}
+            />
+          </div>
+          <div className="featured-column">
+            <FeaturedCard
+              alt={"Black Pillow"}
+              artist={"John Dow"}
+              cost={6.66}
+              image={product}
+              productName={"Incrediblo Black Pillow"}
+            />
+            <FeaturedCard
+              alt={"Black Pillow"}
+              artist={"John Dow"}
+              cost={6.66}
+              image={product}
+              productName={"Incrediblo Black Pillow"}
+            />
+          </div>
+          <div className="featured-column">
+            <FeaturedCard
+              alt={"Black Pillow"}
+              artist={"John Dow"}
+              cost={6.66}
+              image={product}
+              productName={"Incrediblo Black Pillow"}
+            />
+            <FeaturedCard
+              alt={"Black Pillow"}
+              artist={"John Dow"}
+              cost={6.66}
+              image={product}
+              productName={"Incrediblo Black Pillow"}
+            />
+          </div>
+          <div className="featured-column">
+            <FeaturedCard
+              alt={"Black Pillow"}
+              artist={"John Dow"}
+              cost={6.66}
+              image={product}
+              productName={"Incrediblo Black Pillow"}
+            />
+            <FeaturedCard
+              alt={"Black Pillow"}
+              artist={"John Dow"}
+              cost={6.66}
+              image={product}
+              productName={"Incrediblo Black Pillow"}
+            />
+          </div>
+          <div className="featured-column">
+            <FeaturedCard
+              alt={"Black Pillow"}
+              artist={"John Dow"}
+              cost={6.66}
+              image={product}
+              productName={"Incrediblo Black Pillow"}
+            />
+            <FeaturedCard
+              alt={"Black Pillow"}
+              artist={"John Dow"}
+              cost={6.66}
+              image={product}
+              productName={"Incrediblo Black Pillow"}
+            />
+          </div>
+          <div className="featured-column">
+            <FeaturedCard
+              alt={"Black Pillow"}
+              artist={"John Dow"}
+              cost={6.66}
+              image={product}
+              productName={"Incrediblo Black Pillow"}
+            />
+            <FeaturedCard
+              alt={"Black Pillow"}
+              artist={"John Dow"}
+              cost={6.66}
+              image={product}
+              productName={"Incrediblo Black Pillow"}
+            />
           </div>
         </div>
       </div>
