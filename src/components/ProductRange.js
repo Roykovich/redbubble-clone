742a0f87a54345628afb617cbc2f60b83736e7f4
Rubén Ortiz <runerd.menethil@gmail.com>
2021-07-05 20:50:59 -0400

    Add home link to anchors references and remove anchors inside the of another anchors

--- src/components/ProductRange.js
+++ src/components/ProductRange.js
@@ -21,121 +21,121 @@ const ProductRange = () => {
         <div className="shop-range-grid">
           {/* 1st row */}
           <div className="shop-range-item-container">
-            <a href="#">
+            <a href="/">
               <img src={pillow} alt="Shop Pillow" />
+              <div className="shop-button-container">
+                <span>Shop Pillows</span>
+              </div>
             </a>
-            <div className="shop-button-container">
-              <a href="#">Shop Pillows</a>
-            </div>
           </div>
           <div className="shop-range-item-container">
-            <a href="#">
+            <a href="/">
               <img src={stickers} alt="Shop Sticker" />
+              <div className="shop-button-container">
+                <span>Shop Stickers</span>
+              </div>
             </a>
-            <div className="shop-button-container">
-              <a href="#">Shop Stickers</a>
-            </div>
           </div>
           <div className="shop-range-item-container">
-            <a href="#">
+            <a href="/">
               <img src={tshirt} alt="Shop T-shirts" />
+              <div className="shop-button-container">
+                <span>Shop T-Shirts</span>
+              </div>
             </a>
-            <div className="shop-button-container">
-              <a href="#">Shop T-Shirts</a>
-            </div>
           </div>
           {/* 3rd row */}
           <div className="shop-range-item-container">
-            <a href="#">
+            <a href="/">
               <img src={poster} alt="Shop Posters" />
               <div className="shop-button-container">
-                <a href="#">Shop Posters</a>
+                <span>Shop Posters</span>
               </div>
             </a>
           </div>
           <div className="shop-range-item-container">
-            <a href="#">
+            <a href="/">
               <img src={wall_art} alt="Shop Wall Art" />
               <div className="shop-button-container">
-                <a href="#">Shop Wall Art</a>
+                <span>Shop Wall Arts</span>
               </div>
             </a>
           </div>
           <div className="shop-range-item-container">
-            <a href="#">
+            <a href="/">
               <img src={clothing} alt="Shop Clothing" />
               <div className="shop-button-container">
-                <a href="#">Shop Clothing</a>
+                <span>Shop Clothing</span>
               </div>
             </a>
           </div>
           {/* 4th row */}
           <div className="shop-range-item-container">
-            <a href="#">
+            <a href="/">
               <img src={phone_cases} alt="Shop Phone Cases" />
               <div className="shop-button-container">
-                <a href="#">Shop Phone Cases</a>
+                <span>Shop Phone Cases</span>
               </div>
             </a>
           </div>
           <div className="shop-range-item-container">
-            <a href="#">
+            <a href="/">
               <img src={home_and_living} alt="Shop Home & Living" />
               <div className="shop-button-container">
-                <a href="#">Shop Home & Living</a>
+                <span>Shop Home & Living</span>
               </div>
             </a>
           </div>
           <div className="shop-range-item-container">
-            <a href="#">
+            <a href="/">
               <img src={masks} alt="Shop Masks" />
               <div className="shop-button-container">
-                <a href="#">Shop Masks</a>
+                <span>Shop Masks</span>
               </div>
             </a>
           </div>
           {/* 5th row */}
           <div className="shop-range-item-container">
-            <a href="#">
+            <a href="/">
               <img src={accessories} alt="Shop Accessories" />
               <div className="shop-button-container">
-                <a href="#">Shop Accessories</a>
+                <span>Shop Accessories</span>
               </div>
             </a>
           </div>
           <div className="shop-range-item-container">
-            <a href="#">
+            <a href="/">
               <img src={stationery} alt="Shop Stationery" />
               <div className="shop-button-container">
-                <a href="#">Shop Stationery</a>
+                <span>Shop Stationery</span>
               </div>
             </a>
           </div>
           <div className="shop-range-item-container">
-            <a href="#">
+            <a href="/">
               <img src={gifts} alt="Shop Gifts" />
               <div className="shop-button-container">
-                <a href="#">Shop Gifts</a>
+                <span>Shop Gifts</span>
               </div>
             </a>
           </div>
           {/* 2nd row */}
           <div className="shop-range-info">
-            <i class="fa fa-image"></i>
+            <i className="fa fa-image"></i>
             <div>
               <h3>Weirdly meaningful art</h3>
               <p>Millions of designs on over 70 high quality products.</p>
             </div>
           </div>
           <div className="shop-range-info">
-            <i class="fa fa-thumbs-up"></i>
+            <i className="fa fa-thumbs-up"></i>
             <div>
               <h3>Purchases pay artists</h3>
               <p>Money goes directly into a creative person's pocket.</p>
             </div>
           </div>
           <div className="shop-range-info">
-            <i class="fa fa-envira"></i>
+            <i className="fa fa-envira"></i>
             <div>
               <h3>Socially responsible production</h3>
               <p>We're investing in programs to offset all carbon emissions.</p>
