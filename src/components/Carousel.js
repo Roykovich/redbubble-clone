cc199f4d9e226d32885e459503d9151c0ead6f61
Rubén Ortiz <runerd.menethil@gmail.com>
2021-07-12 19:24:15 -0400

    Create modular Carousel component

---
+++ src/components/Carousel.js
@@ -0,0 +1,72 @@
+import { useState, useEffect, useRef } from "react";
+
+import "../styles/Carousel.css";
+
+const Carousel = ({ children, height }) => {
+  // STATE
+  const [index, setIndex] = useState(0);
+  const [slide, setSlide] = useState(0);
+  const [carouselWidth, setCarouselWidth] = useState(0);
+
+  // REFS
+  const carousel = useRef(null);
+  const track = useRef(null);
+  const prevButton = useRef(null);
+  const nextButton = useRef(null);
+
+  const nextButtonClick = () => {
+    setIndex(
+      track.current.offsetWidth - index * carouselWidth < index * carouselWidth
+        ? index
+        : index + 1
+    );
+
+    prevButton.current.classList.add("show-carousel-nav-button");
+  };
+
+  const prevButtonClick = () => {
+    setIndex(index <= 0 ? 0 : index - 1);
+  };
+
+  useEffect(() => {
+    setSlide(index * -carouselWidth);
+  }, [index, carouselWidth]);
+
+  useEffect(() => {
+    setCarouselWidth(carousel.current.offsetWidth);
+
+    window.addEventListener("resize", () => {
+      setCarouselWidth(carousel.current.offsetWidth);
+    });
+  }, []);
+
+  return (
+    <div ref={carousel} style={{ height }} className="carousel">
+      <div
+        ref={track}
+        style={{ transform: `translateX(${slide}px)` }}
+        className="carousel-track"
+      >
+        {children}
+      </div>
+      <div className="carousel-nav">
+        <button
+          onClick={prevButtonClick}
+          ref={prevButton}
+          className="carousel-prev-button"
+        >
+          <i className="fa fa-chevron-left"></i>
+        </button>
+        <button
+          onClick={nextButtonClick}
+          ref={nextButton}
+          className="carousel-next-button"
+        >
+          <i className="fa fa-chevron-right"></i>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Carousel;
