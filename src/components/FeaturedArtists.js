ead9bce4682ed62061756b417c751f43393ad3cc
Rubén Ortiz <runerd.menethil@gmail.com>
2021-07-14 23:28:56 -0400

    Add ArtistCard component

--- src/components/FeaturedArtists.js
+++ src/components/FeaturedArtists.js
@@ -1,7 +1,8 @@
 import { useState, useEffect } from "react";
 import Carousel from "./Carousel";
+import ArtistCard from "./ArtistCard";
 
-import image from "../images/masks.jpg";
+import image from "../images/products/Kurisuvaporwave.jpg";
 import pfp from "../images/gifts.jpg";
 
 import "../styles/FeaturedArtists.css";
@@ -11,19 +12,111 @@ const FeaturedArtists = () => {
     <section>
       <div className="featured-artists-container">
         <h1>Featured artists</h1>
-        <Carousel height="540px">
-          <div className="artist-card">
-            <div>
-              <img src={image} alt="" />
-            </div>
-            <div>
-              <div>
-                <img src={pfp} alt="" />
-              </div>
-              <a href="/">John Doe</a>
-              <a href="/">View Shop</a>
-            </div>
-          </div>
+        <Carousel height="420px">
+          <ArtistCard
+            alt="background"
+            artistName="John Doe"
+            image={image}
+            pfp={pfp}
+            pfpAlt="artist profile picture"
+            style={{
+              height: "100%",
+              width: "calc(((100% + 16px) / 5) - 16px)",
+              minWidth: "calc(((100% + 16px) / 4) - 16px)",
+              marginRight: "16px",
+            }}
+          />
+          <ArtistCard
+            alt="background"
+            artistName="John Doe"
+            image={image}
+            pfp={pfp}
+            pfpAlt="artist profile picture"
+            style={{
+              height: "100%",
+              width: "calc(((100% + 16px) / 5) - 16px)",
+              minWidth: "calc(((100% + 16px) / 4) - 16px)",
+              marginRight: "16px",
+            }}
+          />
+          <ArtistCard
+            alt="background"
+            artistName="John Doe"
+            image={image}
+            pfp={pfp}
+            pfpAlt="artist profile picture"
+            style={{
+              height: "100%",
+              width: "calc(((100% + 16px) / 5) - 16px)",
+              minWidth: "calc(((100% + 16px) / 4) - 16px)",
+              marginRight: "16px",
+            }}
+          />
+          <ArtistCard
+            alt="background"
+            artistName="John Doe"
+            image={image}
+            pfp={pfp}
+            pfpAlt="artist profile picture"
+            style={{
+              height: "100%",
+              width: "calc(((100% + 16px) / 5) - 16px)",
+              minWidth: "calc(((100% + 16px) / 4) - 16px)",
+              marginRight: "16px",
+            }}
+          />
+          <ArtistCard
+            alt="background"
+            artistName="John Doe"
+            image={image}
+            pfp={pfp}
+            pfpAlt="artist profile picture"
+            style={{
+              height: "100%",
+              width: "calc(((100% + 16px) / 5) - 16px)",
+              minWidth: "calc(((100% + 16px) / 4) - 16px)",
+              marginRight: "16px",
+            }}
+          />
+          <ArtistCard
+            alt="background"
+            artistName="John Doe"
+            image={image}
+            pfp={pfp}
+            pfpAlt="artist profile picture"
+            style={{
+              height: "100%",
+              width: "calc(((100% + 16px) / 5) - 16px)",
+              minWidth: "calc(((100% + 16px) / 4) - 16px)",
+              marginRight: "16px",
+            }}
+          />
+          <ArtistCard
+            alt="background"
+            artistName="John Doe"
+            image={image}
+            pfp={pfp}
+            pfpAlt="artist profile picture"
+            style={{
+              height: "100%",
+              width: "calc(((100% + 16px) / 5) - 16px)",
+              minWidth: "calc(((100% + 16px) / 4) - 16px)",
+              marginRight: "16px",
+            }}
+          />
+          <ArtistCard
+            alt="background"
+            artistName="John Doe"
+            image={image}
+            pfp={pfp}
+            pfpAlt="artist profile picture"
+            style={{
+              height: "100%",
+              width: "calc(((100% + 16px) / 5) - 16px)",
+              minWidth: "calc(((100% + 16px) / 4) - 16px)",
+              marginRight: "16px",
+            }}
+          />
         </Carousel>
       </div>
     </section>
