59b0903f79a5ae641e3c5c2a3b297becd104194e
Rubén Ortiz <runerd.menethil@gmail.com>
2021-07-14 23:25:40 -0400

    Create Featured Artist component

--- src/components/ArtistCard.js
+++ src/components/ArtistCard.js
@@ -1,3 +1,24 @@
-const ArtistCard = () => {};
+import "../styles/ArtistCard.css";
+
+const ArtistCard = ({ alt, artistName, image, pfp, pfpAlt, style }) => {
+  return (
+    <div style={style} className="artist-card">
+      <div className="artist-card-image-container">
+        <img src={image} alt={alt} />
+      </div>
+      <div className="artist-card-info-container">
+        <div className="artist-card-info-pfp">
+          <img src={pfp} alt={pfpAlt} />
+        </div>
+        <a className="artist-card-name" href="/">
+          {artistName}
+        </a>
+        <a className="artist-card-button" href="/">
+          View Shop
+        </a>
+      </div>
+    </div>
+  );
+};
 
 export default ArtistCard;
