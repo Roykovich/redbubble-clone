1ee893d5de3d37758f803206369e4c83c4f900be
Rubén Ortiz <runerd.menethil@gmail.com>
2021-07-19 02:35:07 -0400

    Add footer component to the App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar";
 import CategoriesSelector from "./components/CategoriesSelector";
 import LandingPage from "./components/LandingPage";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <CategoriesSelector />
       <LandingPage />
+      <Footer />
     </div>
   );
 }
